b9c70a8cb09fee5ee6a54feaef1d64bea895ab41
ptgmm <ptgmm@proton.me>
2024-06-08 02:45:05 +0200

    Add //github.com/alexjercan nvim config (I will be editing this for my preferences)

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,20 +1,22 @@
-vim.g.mapleader = " "
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
 
-ColorMyColor()
+require("neovim.set")
+require("neovim.remap")
+require("neovim.globals")
+require("neovim.autocmd")
+
+require("lazy").setup({
+    spec = "plugins",
+    change_detection = { notify = false }
+})
